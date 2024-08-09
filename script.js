f295691fed8354bfdd0ebc754284200af6c9845a
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-08-09 18:10:54 +0200

    example input arrays was added

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function iterateAndSum(arr) {
   return arr.reduce((sum, curr) => sum + curr, 0);
 }
+
+// Przykładowy input:
+const input1 = iterateAndSum([1, 2, 3]);
+const input2 = iterateAndSum([10, 10, 10, 10]);
+console.log(input1, input2);
